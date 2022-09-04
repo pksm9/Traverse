c42dc9e5f25ce224dc9850424ec34c255f88759b
Prabod <sandaruwanprabod@gmail.com>
2022-09-04 15:58:35 +0530

    Navigation Drawer item select

---
+++ app/src/main/java/com/example/traverse/PlaceDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.traverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
